ca8d4310ab0359edb7db106fd6da7411f3ec57fa
Themba Dube <embeddedthemba@gmail.com>
2020-09-23 14:57:16 -0400

    Enable all objects

--- lv_conf.h
+++ lv_conf.h
@@ -608,11 +608,11 @@ typedef void * lv_obj_user_data_t;
  * 1: Some extra precision
  * 2: Best precision
  */
-#  define LV_LINEMETER_PRECISE    0
+#  define LV_LINEMETER_PRECISE    2
 #endif
 
 /*Mask (dependencies: -)*/
-#define LV_USE_OBJMASK  0
+#define LV_USE_OBJMASK  1
 
 /*Message box (dependencies: lv_rect, lv_btnm, lv_label)*/
 #define LV_USE_MSGBOX     1
