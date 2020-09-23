415ccc4905140c5dbd54206483f2e545b7a908f2
Themba Dube <embeddedthemba@gmail.com>
2020-09-23 13:43:49 -0400

    Show submodule version in working tree, not in HEAD

--- findtag.sh
+++ findtag.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git ls-tree -r HEAD | grep commit | sed 's/\t/ /g' | cut -d " " -f 3- | while read -r line; do echo -n "$line "; done; echo
+git submodule status | awk '{print $1 " " $2}' | sed 's/+//g' | while read -r line; do echo -n "$line "; done; echo
