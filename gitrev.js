1075cac4ce894ce5532417b13bc5683fed3cf917
embeddedt <embeddedt@users.noreply.github.com>
2020-11-27 20:07:49 +0000

    Deploying to gh-pages from  @ 815e07035ff5a8d9f2c66433d8071bca60300010 🚀

--- gitrev.js
+++ gitrev.js
@@ -1,1 +1,1 @@
-window.git_hash = "0b432a637a5c406841b1902146a937eed2082099 lv_drivers 25ae1dc9842b23d3c27effcd1b1a98c7e8688151 lv_examples 9797c555c01d1aa2de1de057973a1a4bbbd9599f lvgl ";
+window.git_hash = "0b432a637a5c406841b1902146a937eed2082099 lv_drivers 25ae1dc9842b23d3c27effcd1b1a98c7e8688151 lv_examples 3c55dfb672a4f5eb3c42576d13993f902ec5055c lvgl ";
