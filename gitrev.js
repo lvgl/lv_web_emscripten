0c92c839c58960631bc93405d56a8f8b1ce0d9f1
kisvegabor <kisvegabor@gmail.com>
2021-01-19 14:05:47 +0000

    Deploying to gh-pages from  @ 15c5f596859b509ea96e250411deea9cffbc7ed1 🚀

--- gitrev.js
+++ gitrev.js
@@ -1,1 +1,1 @@
-window.git_hash = "4b0ec4d1d852436c2498331ec877f5721965194d lv_drivers 318be44c7273df276676361fe23da47c8cedc4ea lv_examples 60be7c1ceecf26bcc7413e80ddb2f154a5f6b5e5 lvgl ";
+window.git_hash = "85f531a8f764d11d9d188a1c8c59e8ecf4273238 lv_drivers a4542262c2fd4828714b2e673e968f57a452f4f9 lv_examples 011f105038212fff7b4aeec9a81ea0a0da7ded0b lvgl ";
