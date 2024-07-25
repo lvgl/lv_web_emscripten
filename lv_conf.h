92610364ca29835b3f0fcf79b9f1ae671667535e
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-07-25 16:23:21 +0200

    Update lv_conf.h
    Enable MATRIX

--- lv_conf.h
+++ lv_conf.h
@@ -398,7 +398,11 @@
 #define LV_ATTRIBUTE_EXTERN_DATA
 
 /* Use `float` as `lv_value_precise_t` */
-#define LV_USE_FLOAT            0
+#define LV_USE_FLOAT            1
+
+/*Enable matrix support
+ *Requires `LV_USE_FLOAT = 1`*/
+#define LV_USE_MATRIX           1
 
 /*==================
  *   FONT USAGE
