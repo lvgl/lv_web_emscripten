910cbfef67ee08658034dcdce3e56149da9be51e
liamHowatt <liamjmh0@gmail.com>
2024-08-22 15:38:44 -0400

    keep memfs, set drive letter

--- lv_conf.h
+++ lv_conf.h
@@ -673,9 +673,9 @@
 #endif
 
 /*API for memory-mapped file access. */
-#define LV_USE_FS_MEMFS 0
+#define LV_USE_FS_MEMFS 1
 #if LV_USE_FS_MEMFS
-    #define LV_FS_MEMFS_LETTER '\0'     /*Set an upper cased letter on which the drive will accessible (e.g. 'A')*/
+    #define LV_FS_MEMFS_LETTER 'M'     /*Set an upper cased letter on which the drive will accessible (e.g. 'A')*/
 #endif
 
 /*API for LittleFs. */
