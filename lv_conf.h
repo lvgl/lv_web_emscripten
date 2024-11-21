48eb3b1636b967534554c68b1bf0d00a7df6fe1a
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2024-11-21 09:39:39 +0100

    update lv_conf

--- lv_conf.h
+++ lv_conf.h
@@ -1234,7 +1234,7 @@
 #endif
 
 /** Flex layout demo */
-#define LV_USE_DEMO_FLEX_LAYOUT     0
+#define LV_USE_DEMO_FLEX_LAYOUT     1
 
 /** Smart-phone like multi-language demo */
 #define LV_USE_DEMO_MULTILANG       1
